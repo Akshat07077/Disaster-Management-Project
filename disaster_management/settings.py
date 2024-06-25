3774a31ac8b68a7a6363dd8b4a4754def7020fa4
Akshat07077 <messageakshat@gmail.com>
2024-06-25 23:54:29 +0530

    Update settings.py

--- disaster_management/settings.py
+++ disaster_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t$(5)=s_^-gx#71g^$xdenvkn(5bh9p-(p7_wt(v+2wjs$g*3b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
