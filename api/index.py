475d1d4f68b388136d41074abd817219c553a7a0
Akshat Sharma <messageakshat@gmail.com>
2024-06-28 03:12:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,13 @@ import sys
 
 # Adjust the path to your Django app
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import { createRequire } from 'node:module';
+
+let sayHello = createRequire(import.meta.url)('../db.sqlite3');
+
+export function GET(request) {
+  return new Response(sayHello());
+}
 
 # Set the environment variable for Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disaster_management.settings')
